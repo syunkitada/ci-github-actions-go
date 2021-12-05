938b475e162d57290a4be187ae5cd1a87bcc28e4
syunkitada <syun.kitada@gmail.com>
2021-12-05 17:22:19 +0900

    update

--- ci/actions.sh
+++ ci/actions.sh
@@ -1,38 +1,38 @@
 #!/bin/bash -ex
 
 COMMAND="${@:-help}"
-
 NAME=${PWD##*/}
 WD=${PWD}
-MODULE=`grep '^module ' go.mod | awk '{print $2}'`
-GOPATH=/opt/ci/go-1.17.4
-MODULE_PATH=/opt/ci/go-1.17.4/${MODULE}
 
 function setup_go() {
+    GOPATH=/opt/ci/go-1.17.4
+    MODULE=`grep '^module ' go.mod | awk '{print $2}'`
+    MODULE_PATH=/opt/ci/go-1.17.4/src/${MODULE}
+
 	mkdir -p /opt/ci
 	test -e /opt/ci/go || \
-        (wget https://go.dev/dl/go1.17.4.linux-amd64.tar.gz && tar xvf go1.17.4.linux-amd64.tar.gz -C /opt/ci/ && rm -f go1.17.4.linux-amd64.tar.gz)
+        (wget -q https://go.dev/dl/go1.17.4.linux-amd64.tar.gz && tar xf go1.17.4.linux-amd64.tar.gz -C /opt/ci/ && rm -f go1.17.4.linux-amd64.tar.gz)
 
-    mkdir -p /opt/ci/gcov2lcov
-	test -e /opt/ci/gcov2lcov/bin || \
-        (wget https://github.com/jandelgado/gcov2lcov/releases/latest/download/gcov2lcov-linux-amd64.tar.gz && \
-         tar xvf gcov2lcov-linux-amd64.tar.gz -C /opt/ci/gcov2lcov && rm -f gcov2lcov-linux-amd64.tar.gz)
-
-    test -e $GOPATH/src/$MODULE || \
-        (mkdir -p $GOPATH/src/$MODULE && cp -r $WD/. $GOPATH/src/$MODULE)
+    test -e $MODULE_PATH || \
+        (mkdir -p $MODULE_PATH && cp -r $WD/. $MODULE_PATH)
 
     cat << EOS > /opt/ci/envgorc
-PATH=$PATH:/opt/ci/go:/opt/ci/gcov2lcov
-GOPATH=$GOPATH
+PATH=/opt/ci/go/bin:$PATH
+GOROOT=/opt/ci/go
+GOPATH=${GOPATH}
+GO111MODULE=off
 MODULE_PATH=$MODULE_PATH
 EOS
+
+	. /opt/ci/envgorc
+    go install github.com/jandelgado/gcov2lcov@latest
 }
 
 function test_go() {
 	. /opt/ci/envgorc
     cd ${MODULE_PATH}
     go test --coverpkg ./pkg/... -coverprofile=.coverage.out ./pkg/...
-	gcov2lcov-linux-amd64 -infile .coverage.out -outfile /tmp/coverage.lcov
+    $GOPATH/bin/gcov2lcov-linux-amd64 -infile .coverage.out -outfile /tmp/coverage.lcov
 }
 
 $COMMAND
